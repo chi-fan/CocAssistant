2f21870bff74f228f266be662ee7a236e8ca365f
HoneyGump <honeygump@qq.com>
2020-02-06 17:32:23 +0800

    test

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("hello woirld")
+import pytesseract
+from PIL import Image
+# pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR';
+image = Image.open('D:\\COC_support_py\\COC-support\\google.png');
+code = pytesseract.image_to_string(image, lang = 'chi_sim');
+print(code);
